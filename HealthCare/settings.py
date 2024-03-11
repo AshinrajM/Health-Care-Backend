0b01bb03e8a9060fd299f7da16b784240b6a4217
AshinrajM <ashinrajeevank@outlook.com>
2024-03-11 22:48:35 +0530

    .

--- HealthCare/settings.py
+++ HealthCare/settings.py
@@ -148,8 +148,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
 }
 
 CORS_ALLOWED_ORIGINS = [
