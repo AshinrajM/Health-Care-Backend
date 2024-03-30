c7bf20e60bb178e65e73e3d310ac4676a81dd15f
AshinrajM <ashinrajeevank@outlook.com>
2024-03-30 12:00:42 +0530

    date field in model changed its constraint

--- booking/models.py
+++ booking/models.py
@@ -4,9 +4,6 @@ from accounts.models import Associate
 
 class Available(models.Model):
     associate = models.ForeignKey(Associate, on_delete=models.CASCADE)
-    date = models.DateField()
+    date = models.DateField(unique=True)
     is_morning = models.BooleanField(default=False)
     is_noon = models.BooleanField(default=False)
-
-    # def __str__(self):
-    #     return self.date
