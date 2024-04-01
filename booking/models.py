1c9c9254fc98057e74925b24908daba43f27faf4
AshinrajM <ashinrajeevank@outlook.com>
2024-04-01 17:06:24 +0530

    changed the unique status of date field in available

--- booking/models.py
+++ booking/models.py
@@ -4,6 +4,6 @@ from accounts.models import Associate
 
 class Available(models.Model):
     associate = models.ForeignKey(Associate, on_delete=models.CASCADE)
-    date = models.DateField(unique=True)
+    date = models.DateField()
     is_morning = models.BooleanField(default=False)
     is_noon = models.BooleanField(default=False)
