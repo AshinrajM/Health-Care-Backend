0f73922ae1ecfd88f1dd229eaa409c482a72b94b
AshinrajM <ashinrajeevank@outlook.com>
2024-05-08 09:17:55 +0530

    testing for websocket connection tested

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,19 +2,21 @@ from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 from django.core.exceptions import ValidationError
 from json import loads
+import json
+
 
 class Chatroom_consumer(WebsocketConsumer):
 
     def connect(self):
         self.accept()
         print("connected")
-        self.send(text_data="WebSocket Connection Successful!")
+        self.send(text_data=json.dumps({"message": "WebSocket Connection Successful!"}))
 
     def disconnect(self, close_code):
         pass
 
     def receive(self, text_data):
         data = loads(text_data)
-        message = data.get('message')
-        print(message,"received")
+        message = data.get("message")
+        # print(message,"received")
         pass
