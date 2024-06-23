8b616e60f3728bf964de07d36383917be0f06067
AshinrajM <ashinrajeevank@outlook.com>
2024-06-23 12:23:50 +0530

    installed packages added to requirement.txt

--- HealthCare/settings.py
+++ HealthCare/settings.py
@@ -172,8 +172,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     # ----------------
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
